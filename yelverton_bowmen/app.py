b9149c9c76b82eae9658ace9fa0f766b5c287834
Corey Richardson <richardsoncorey04@gmail.com>
2023-04-26 12:27:28 +0000

    Records script in place, still need to migrate data over

--- yelverton_bowmen/app.py
+++ yelverton_bowmen/app.py
@@ -2,15 +2,6 @@ from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-from commitee import committee_members
-
-from static.records.barebow import barebow
-from static.records.recurve import recurve
-from static.records.compound import compound
-from static.records.longbow import longbow
-from static.records.afb import afb
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
@@ -24,17 +15,31 @@ def index():
     return render_template("index.html", 
                            subnav_bar=index_subnav)
 
+from commitee import committee_members
+
 @app.route('/club', methods = ["GET","POST"])
 def club():
     return render_template("club.html", 
                            subnav_bar=club_subnav, committee_members=committee_members)
 
-bowtype_pointers = [recurve, barebow, compound, longbow, afb]
+
+from static.records.barebow import barebow
+from static.records.recurve import recurve
+from static.records.compound import compound
+from static.records.longbow import longbow
+from static.records.afb import afb
+
+bowtypes = {"Barebow" : barebow,
+            "Recurve" : recurve,
+            "Compound" : compound,
+            "Longbow" : longbow,
+            "American Flatbow" : afb}
 
 @app.route('/records', methods = ["GET","POST"])    
 def records():
     return render_template("records.html",
                            subnav_bar=records_subnav,
+                           bowtypes=bowtypes,
                            barebow=barebow,
                            recurve=recurve,
                            compound=compound,
--- yelverton_bowmen/static/records/compound.py
+++ yelverton_bowmen/static/records/compound.py
@@ -1,28 +1,6 @@
 # Format
 # Round Name : [Male_Name, Male_Score, Female Name, Female Score]
 
-# <table>
-# { for name , record_data in dict.items() }
-# <tr>
-# <td>
-# round name
-# </td>
-# <td>
-# male name
-# </td>
-# <td>
-# male score
-# </td>
-# <td>
-# female name
-# </td>
-# <td>
-# female name
-# </td>
-# </tr>
-# { endfor }
-# </table>
-
 compound = {
-    "Warwick 40" : ["Corey Richardson","414","",""]
+    "Warwick 40" : ["Corey Richardson","414","",""] ,
 }
