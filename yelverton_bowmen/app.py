995b71f73369a1d5f7943166633f28cb26414745
Corey Richardson <richardsoncorey04@gmail.com>
2023-04-26 09:05:43 +0000

    Added committee section

--- yelverton_bowmen/app.py
+++ yelverton_bowmen/app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+from commitee import committee_members
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
 @app.route('/', methods = ["GET","POST"])
 def index():
     return render_template("index.html")
+
+@app.route('/club', methods = ["GET","POST"])
+def club():
+    return render_template("club.html", committee_members=committee_members)
---
+++ yelverton_bowmen/commitee.py
@@ -0,0 +1,65 @@
+committee_members = {
+        "Linda Wright" : 
+            ["Chairman", 
+             "static/imgs/committee-members/chairman.jpg", 
+             "The Chairman is the ‘manager’ of the club and the committee. They lead committee meetings and are responsible for coordination of committee tasks."] ,
+        "Dave Leader" : 
+            ["Vice Chairman", 
+             "static/imgs/committee-members/vice-chairman.jpg", 
+             "The Vice-Chairman takes on the Chairman’s responsibilities when they are not available."] ,
+        "Nick Tripp" :
+            ["Secretary", 
+             "static/imgs/committee-members/secretary.jpg",
+             "The Secretary is normally the point of contact for those outside the club. They are the record keeper for committee meetings."] ,
+        "Jamie Pruden" :
+            ["Treasurer", 
+             "static/imgs/committee-members/treasurer.jpg", 
+             "Responsible for administering the club’s finances."] ,
+        "Graeme Bull" :
+            ["Membership Secretary", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Responsible for processing new member applications and the renewal of existing memberships. Typically works closely with the Treasurer."] ,
+        "John Pengilley" :
+            ["Records Officer", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Collects scored rounds and issues classifications, records, etc as required. They also manage the club’s contribution to postal leagues."] ,
+        "Paul Warren" :
+            ["Equipment Officer", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Responsible for the acquisition and maintenance of club equipment."] ,
+        "Iain Lees" :
+            ["Training Co-ordinator", 
+             "static/imgs/committee-members/training.jpg", 
+             "Responsible for the organisation and running of beginners courses. They are a point of contact for members of the public wishing to join the club."] ,
+        "Bruce Cox" :
+            ["Safeguarding and Welfare Officer", 
+             "static/imgs/committee-members/empty.jpg", 
+             "In the unlikely event of a member’s welfare being threatened the SWO would be the point of contact for those concerned."] ,
+        "Corey Richardson" :
+            ["Webmaster", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Responsible for the creation and maintenance of the club’s online presence."] ,
+        "VACANT" :
+            ["Archivist", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Collator and keeper of historical records relating to the club and its members."] ,
+        "Karen MacFirbisigh" :
+            ["Public Relations Officer", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Generally responsible for outgoing correspondence, including contact with local newspapers and television to inform them of activities, such as hosting tournaments. They also contact archery publications with stories of interest. They may also be the point of contact at public events such as county shows and have-a-goes."] ,
+        "Becky Lonton / Jenny Kerchey" :
+            ["Junior Rep", 
+             "static/imgs/committee-members/junior.jpg", 
+             "The voice of the junior members to the committee. They also run junior specific classifications and postal leagues that aren’t run by the records officer."] ,
+        "VACANT" :
+            ["Tournament Officer", 
+             "static/imgs/committee-members/empty.jpg", 
+             "Responsible for organisation and running of tournaments hosted by the club, as well as those hosted on behalf of other bodies."] ,
+        "Iain Lees" :
+            ["Social Secretary", 
+             "static/imgs/committee-members/social.jpg", 
+             "Responsible for organising social events for the club, such as Christmas meals, fun shoots, day trips, etc."]
+        }
+
+        # "" :
+        #     ["", "", ""] ,
