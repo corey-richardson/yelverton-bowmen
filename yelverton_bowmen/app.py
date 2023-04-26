851adf29c8da437876b7ec07d1bcb7d9aa531da3
Corey Richardson <richardsoncorey04@gmail.com>
2023-04-26 10:04:00 +0000

    Modified subnav bar to be more modular

--- yelverton_bowmen/app.py
+++ yelverton_bowmen/app.py
@@ -7,10 +7,16 @@ from commitee import committee_members
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
+# SET SUBNAV LINK LABELS HERE
+index_subnav = ["Welcome", "Beginners", "Useful Links"]
+club_subnav = ["Committee"]
+
 @app.route('/', methods = ["GET","POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", 
+                           subnav_bar=index_subnav)
 
 @app.route('/club', methods = ["GET","POST"])
 def club():
-    return render_template("club.html", committee_members=committee_members)
+    return render_template("club.html", 
+                           subnav_bar=club_subnav, committee_members=committee_members)
