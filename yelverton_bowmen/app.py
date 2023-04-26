459647780e13296ee3a8dff9292f8d72b8856834
Corey Richardson <richardsoncorey04@gmail.com>
2023-04-26 14:24:49 +0000

    added gallery functionality

--- yelverton_bowmen/app.py
+++ yelverton_bowmen/app.py
@@ -2,39 +2,47 @@ from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+# CLUB PAGE
+from commitee import committee_members
+
+# RECORDS ROOM PAGE
+from static.records.barebow import barebow
+from static.records.recurve import recurve
+from static.records.compound import compound
+from static.records.longbow import longbow
+from static.records.afb import afb
+bowtypes = {"Barebow" : barebow,
+            "Recurve" : recurve,
+            "Compound" : compound,
+            "Longbow" : longbow,
+            "American Flatbow" : afb }
+
+# GALLERY PAGE
+from os import scandir, path
+directory = 'static/gallery'
+
+# FLASK APP SETUP
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
 # SET SUBNAV LINK LABELS HERE
 index_subnav = ["Welcome", "Beginners", "Useful Links"]
 club_subnav = ["Committee"]
-records_subnav = ["Barebow","Recurve","Compound","Longbow","AFB"]
+records_subnav = ["Club Records", "Rounds and Classifications", "252 Scheme", "Frostbite Postal", "Portsmouth Postal"]
 
+# INDEX PAGE
 @app.route('/', methods = ["GET","POST"])
 def index():
     return render_template("index.html", 
                            subnav_bar=index_subnav)
 
-from commitee import committee_members
-
+# CLUB PAGE
 @app.route('/club', methods = ["GET","POST"])
 def club():
     return render_template("club.html", 
                            subnav_bar=club_subnav, committee_members=committee_members)
 
-
-from static.records.barebow import barebow
-from static.records.recurve import recurve
-from static.records.compound import compound
-from static.records.longbow import longbow
-from static.records.afb import afb
-
-bowtypes = {"Barebow" : barebow,
-            "Recurve" : recurve,
-            "Compound" : compound,
-            "Longbow" : longbow,
-            "American Flatbow" : afb}
-
+# RECORDS ROOM PAGE
 @app.route('/records', methods = ["GET","POST"])    
 def records():
     return render_template("records.html",
@@ -45,3 +53,9 @@ def records():
                            compound=compound,
                            longbow=longbow,
                            afb=afb)
+    
+@app.route('/gallery', methods = ["GET","POST"]) 
+def gallery():
+    return render_template("gallery.html",
+                           directory=directory,
+                           scandir=scandir)
--- yelverton_bowmen/static/records/compound.py
+++ yelverton_bowmen/static/records/compound.py
@@ -1,5 +1,4 @@
-# Format
-# Round Name : [Male_Name, Male_Score, Female Name, Female Score]
+# { round_name : ["gents_name", "gents_score", "ladies_name", "ladies_score"] }
 
 compound = {
     "Warwick 40" : ["Corey Richardson","414","",""] ,
